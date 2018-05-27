cdf7d67d383b0a6fbd626cb28dc1e5cae8924952
Ryan May <xtheraingoddessx@gmail.com>
2018-05-27 16:45:24 -0700

    added main method to project

---
+++ src/com/rain/app/spelltracker/main/Main.java
@@ -0,0 +1,16 @@
+package com.rain.app.spelltracker.main;
+
+import com.rain.app.spelltracker.gui.ApplicationWindow;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		ApplicationWindow app = new ApplicationWindow();
+		app.begin(args);
+	}
+
+}
